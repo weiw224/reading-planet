5fce87b8d9b5671f65617b76e43d508e01972d7f
emily <emily@emilydeMacBook-Pro.local>
2026-02-07 14:04:00 +0800

    Create layout components: DefaultLayout and BlankLayout

---
+++ admin/src/layouts/BlankLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="blank-layout">
+    <router-view />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.blank-layout {
+  min-height: 100vh;
+}
+</style>
